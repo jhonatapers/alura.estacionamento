b724ef37e11bfebf5522bbfeae6898c1fbc4227b
Jhonata Peres <jhonatapers@gmail.com>
2022-07-19 20:55:17 -0300

    Adiciona primeiros testes

--- Alura.Estacionamento.Testes/VeiculoTestes.cs
+++ Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -11,6 +11,15 @@ namespace Alura.Estacionamento.Testes
         {
             var veiculo = new Veiculo();
             veiculo.Acelerar(10);
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Frear(10);
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
         }
     }
 }
